9777398873bee9feb6e8650fe53e0e90b41761c9
Andreas Gal <andreas.gal@gmail.com>
2015-04-15 17:50:39 +0800

    improve test

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 var fft = require('./index').fft;
 
-fft(Float32Array([3, 0, 2, 0, 1, 0, 1, 0]), Float32Array(8), function (err, result) {
-  console.log(result);
+fft(Float32Array([1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0]), Float32Array(16), function (err, result) {
+  console.log(Array.prototype.join.call(result, ','));
 });
 
-fft(Float32Array([3, 2, 1, 1]), Float32Array(8), function (err, result) {
-  console.log(result);
+fft(Float32Array([1, 2, 3, 4, 5, 6, 7, 8]), Float32Array(16), function (err, result) {
+  console.log(Array.prototype.join.call(result, ','));
 });
